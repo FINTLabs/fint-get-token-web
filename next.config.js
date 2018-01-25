c09107acc296440d23e555c8527f199813d86017
Jonas Enge <jonas.enge@gmail.com>
2018-01-25 17:41:46 +0100

    Disable webpack

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const glob = require('glob')
 
+module.exports = {
+  webpack (cfg) {
+    cfg.plugins = cfg.plugins.filter(plugin => plugin.constructor.name !== 'UglifyJsPlugin')
+    return cfg
+  }
+}
+
 exports.exportPathMap = () => {
   const pathMap = {}
   glob.sync('pages/**/*.js', { ignore: 'pages/_document.js' }).forEach(s => {
